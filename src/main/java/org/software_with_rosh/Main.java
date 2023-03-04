a60b71c04173fa35496364fe3172642142288e89
roshnee99 <rs171999@gmail.com>
2023-03-03 16:34:39 -0800

    Joe from branch_2

--- src/main/java/org/software_with_rosh/Main.java
+++ src/main/java/org/software_with_rosh/Main.java
@@ -2,6 +2,7 @@ package org.software_with_rosh;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("This is Joe from branch_2!");
         System.out.println("Hello world!");
     }
 }
