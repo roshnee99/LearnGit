03c0c1f3bc2067cf9c49c360b4779d3d166fc9ef
roshnee99 <rs171999@gmail.com>
2023-03-03 16:32:32 -0800

    Add a new print statement to Main

--- src/main/java/org/software_with_rosh/Main.java
+++ src/main/java/org/software_with_rosh/Main.java
@@ -3,5 +3,6 @@ package org.software_with_rosh;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is me from branch 1!");
     }
 }
