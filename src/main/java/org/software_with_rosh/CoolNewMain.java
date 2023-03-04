8a369cb16e6983fc358f6cfa993fda649014fdb5
roshnee99 <rs171999@gmail.com>
2023-03-03 16:05:01 -0800

    Add a gitignore file

---
+++ src/main/java/org/software_with_rosh/CoolNewMain.java
@@ -0,0 +1,9 @@
+package org.software_with_rosh;
+
+public class CoolNewMain {
+
+    public static void main(String[] args) {
+        int sum = 2 + 2;
+        System.out.println("2 + 2 is " + sum);
+    }
+}
---
+++ src/main/java/org/software_with_rosh/EvenCoolerMain.java
@@ -0,0 +1,10 @@
+package org.software_with_rosh;
+
+public class EvenCoolerMain {
+
+    public static void main(String[] args) {
+        int product = 3 * 3;
+        System.out.println("3 * 3 is " + product);
+    }
+
+}
